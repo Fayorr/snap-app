b34c6afb69786471224ca19f9cc22572cc7ad788
fayorr <fayokunmiosho@gmail.com>
2024-06-08 23:17:23 +0100

    navbar

---
+++ app.js
@@ -0,0 +1,9 @@
+// const hamburger = document.querySelector('.hamburger');
+// const navigation = document.querySelector('nav--section');
+
+// function reveal() {
+//     console.log("hello")
+//     navigation.classList.toggle("active");
+//     // menu.classList.toggle("active");
+// }
+// hamburger.addEventListener("click", reveal);
